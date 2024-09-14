2c1f24b5d8b7265000bd07aee6d21594794e4c03
Andrew Trask <liamtrask@gmail.com>
2024-09-14 18:13:43 -0400

    Only watch and run in your own datasite

--- syftbox/client/plugins/watch_and_run.py
+++ syftbox/client/plugins/watch_and_run.py
@@ -16,14 +16,20 @@ def run(shared_state):
 
     datasites = get_datasites(shared_state.client_config.sync_folder)
     for datasite in datasites:
+        # only run on our own datasite
+        if datasite != shared_state.client_config.email:
+            continue
+
         # get the top level perm file
         datasite_path = os.path.join(shared_state.client_config.sync_folder, datasite)
         perm_tree = PermissionTree.from_path(datasite_path)
 
-        runners = list(Path(shared_state.client_config.sync_folder).rglob("*run.sh"))
+        runners = list(Path(datasite_path).rglob("*run.sh"))
         for runr in runners:
             run_from = str(runr.parent)
             perm = perm_tree.permission_for_path(str(runr))
             
             if len(perm.write) == 1 and perm.write[0] == shared_state.client_config.email:
+                print("Running for:" + str(perm.write))
+                print("From:" + run_from)
                 os.system("cd "+run_from+"; sh run.sh")
