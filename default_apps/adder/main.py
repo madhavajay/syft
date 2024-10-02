1d1bbdddc062245c66bd91c534c47c72f31ac711
Andrew Trask <liamtrask@gmail.com>
2024-10-02 00:17:50 -0400

    Fix bug in adder

--- default_apps/adder/main.py
+++ default_apps/adder/main.py
@@ -6,13 +6,13 @@ from syftbox.lib import ClientConfig
 config_path = os.environ.get("SYFTBOX_CLIENT_CONFIG_PATH", None)
 client_config = ClientConfig.load(config_path)
 
-input_folder = f"{client_config.sync_folder}/app_pipelines/adder/inputs/"
-output_folder = f"{client_config.sync_folder}/app_pipelines/adder/done/"
+input_folder = f"{client_config.sync_folder}/{client_config.email}/app_pipelines/adder/inputs/"
+output_folder = f"{client_config.sync_folder}/{client_config.email}/app_pipelines/adder/done/"
 os.makedirs(input_folder, exist_ok=True)
 os.makedirs(output_folder, exist_ok=True)
 
-input_file_path = f"{input_folder}/data.json"
-output_file_path = f"{output_folder}/data.json"
+input_file_path = f"{input_folder}data.json"
+output_file_path = f"{output_folder}data.json"
 
 if os.path.exists(input_file_path):
     with open(input_file_path, "r") as f:
