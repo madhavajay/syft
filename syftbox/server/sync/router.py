acd20aa50105c9bd09558cc6a99f0a5fffa178ea
Aziz Berkay Yesilyurt <abyesilyurt@gmail.com>
2024-10-21 09:36:59 +0300

    add comment

--- syftbox/server/sync/router.py
+++ syftbox/server/sync/router.py
@@ -114,9 +114,3 @@ def apply_diffs(
     return ApplyDiffResponse(
         path=req.path, current_hash=sha256, previous_hash=metadata.hash
     )
-
-
-if __name__ == "__main__":
-    import uvicorn
-
-    uvicorn.run(router, host="127.0.0.1", port=8000)
