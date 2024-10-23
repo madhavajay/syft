a347cfd7d4c4d013b962ea86f25e2b02f0cfccc1
eelcovdw <eelcovdw@gmail.com>
2024-10-23 10:07:40 +0200

    fix tests

--- tests/server/endpoint_test.py
+++ tests/server/endpoint_test.py
@@ -1,23 +1,8 @@
 from fastapi.testclient import TestClient
 
-from tests.server.conftest import TEST_DATASITE_NAME
-
 
 def test_register(client: TestClient):
     data = {"email": "test@example.com"}
     response = client.post("/register", json=data)
     assert response.status_code == 200
     assert "token" in response.json()
-
-    response = client.get("/list_datasites")
-    assert response.status_code == 200
-
-
-def test_list_datasites(client: TestClient):
-    response = client.get("/list_datasites")
-    assert response.status_code == 200
-
-    assert len(response.json()["datasites"])
-
-    response = client.get(f"/datasites/{TEST_DATASITE_NAME}/")
-    assert response.status_code == 200
