571af3b80e8ea460dcbbf869fd024eadff1bf699
Yash Gorana <yash.gorana@hotmail.com>
2024-11-05 23:19:22 +0530

    bye scripts

--- scripts/live_andrew.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-uv run syftbox/client/client.py --config_path=./users/live_andrew.json --sync_folder=./users/live_andrew --email=andrew@openmined.org --port=8082  --server=https://syftbox.openmined.org
--- scripts/symlink.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-mkdir -p users/madhava/madhava@openmined.org/a
-echo '{"admin": ["madhava@openmined.org"], "read": ["madhava@openmined.org", "GLOBAL"], "write": ["madhava@openmined.org"], "filepath": null}' > users/madhava/madhava@openmined.org/a/_.syftperm
-echo "aaa" > users/madhava/madhava@openmined.org/a/a.txt
-
-mkdir -p users/madhava/madhava@openmined.org/a/b
-ln -s users/madhava/madhava@openmined.org/a/a.txt users/madhava/madhava@openmined.org/a/b/b.txt
-
