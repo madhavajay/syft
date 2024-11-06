16f2043cb2ec40b34be2aa8dc7fa105857e5201f
Aziz Berkay Yesilyurt <abyesilyurt@gmail.com>
2024-11-06 16:22:11 +0100

    add locust task

--- tests/stress/locustfile.py
+++ tests/stress/locustfile.py
@@ -1,7 +1,12 @@
-from locust import FastHttpUser, between, task
-from loguru import logger
+import uuid
+from pathlib import Path
 
-from syftbox.client.plugins.sync import endpoints
+from locust import FastHttpUser, between, task
+
+from syftbox.client.plugins.sync import consumer, endpoints
+from syftbox.server.sync.hash import hash_file
+
+file_name = Path("loadtest.txt")
 
 
 class SyftBoxUser(FastHttpUser):
@@ -14,20 +19,51 @@ class SyftBoxUser(FastHttpUser):
         self.email = "aziz@openmined.org"
         self.remote_state: dict[str, list[endpoints.FileMetadata]] = {}
 
-    @task
-    def sync_datasites(self):
-        remote_datasite_states = endpoints.get_datasite_states(
-            self.client,
-            email=self.email,
-        )
-        # logger.info(f"Syncing {len(remote_datasite_states)} datasites")
-        all_files = []
-        for email, remote_state in remote_datasite_states.items():
-            all_files.extend(remote_state)
+        # patch client for update_remote function
+        self.client.sync_folder = Path(self.email)
+        self.client.server_client = self.client
 
-        all_paths = [str(f.path) for f in all_files][:10]
-        logger.info(f"Downloading {len(all_paths)} files")
-        endpoints.download_bulk(
+        self.filepath = self.init_file()
+
+    def init_file(self) -> Path:
+        # create a file on local and send to server
+        filepath = self.client.sync_folder / file_name
+        filepath.parent.mkdir(parents=True, exist_ok=True)
+        filepath.touch()
+        filepath.write_text(uuid.uuid4().hex)
+        local_syncstate = hash_file(filepath.absolute(), root_dir=filepath.parent.absolute())
+        try:
+            endpoints.create(self.client, local_syncstate.path, filepath.read_bytes())
+        except endpoints.SyftServerError:
+            pass
+        return filepath
+
+    # @task
+    # def sync_datasites(self):
+    #     remote_datasite_states = endpoints.get_datasite_states(
+    #         self.client,
+    #         email=self.email,
+    #     )
+    #     # logger.info(f"Syncing {len(remote_datasite_states)} datasites")
+    #     all_files = []
+    #     for email, remote_state in remote_datasite_states.items():
+    #         all_files.extend(remote_state)
+
+    #     all_paths = [str(f.path) for f in all_files][:10]
+    #     logger.info(f"Downloading {len(all_paths)} files")
+    #     endpoints.download_bulk(
+    #         self.client,
+    #         all_paths,
+    #     )
+
+    @task
+    def apply_diff(self):
+        self.filepath.write_text(uuid.uuid4().hex)
+        local_syncstate = hash_file(self.filepath, root_dir=self.client.sync_folder)
+        remote_syncstate = endpoints.get_metadata(self.client, local_syncstate.path)
+
+        consumer.update_remote(
             self.client,
-            all_paths,
+            local_syncstate=local_syncstate,
+            remote_syncstate=remote_syncstate,
         )
