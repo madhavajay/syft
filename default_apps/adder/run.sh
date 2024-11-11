c85b8b4028d36143a795a99d70982d7bdc605676
Yash Gorana <yash.gorana@hotmail.com>
2024-11-12 00:38:33 +0530

    fix pip install

--- default_apps/adder/run.sh
+++ default_apps/adder/run.sh
@@ -5,7 +5,10 @@ set -e
 if [ ! -d .venv ]; then
     uv venv
 fi
-uv pip install --upgrade syftbox
 . .venv/bin/activate
-python main.py
+
+uv pip install --upgrade syftbox
+
+echo "Running 'adder' with $(python3 --version) at '$(which python3)'"
+python3 main.py
 deactivate
