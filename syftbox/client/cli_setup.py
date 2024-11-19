3cee0f9abe9f1f687b41f9cdc6c282f41c5332b2
eelcovdw <eelcovdw@gmail.com>
2024-11-19 14:48:16 +0100

    split client auth cli

--- syftbox/client/cli_setup.py
+++ syftbox/client/cli_setup.py
@@ -6,12 +6,11 @@ import json
 import shutil
 from pathlib import Path
 
-import httpx
-import typer
 from rich import print as rprint
 from rich.prompt import Confirm, Prompt
 
 from syftbox.__version__ import __version__
+from syftbox.client.auth import authenticate_user
 from syftbox.client.client2 import METADATA_FILENAME
 from syftbox.lib.client_config import SyftClientConfig
 from syftbox.lib.constants import DEFAULT_DATA_DIR
@@ -19,6 +18,11 @@ from syftbox.lib.exceptions import ClientConfigException
 from syftbox.lib.validators import DIR_NOT_EMPTY, is_valid_dir, is_valid_email
 
 __all__ = ["setup_config_interactive"]
+
+
+def is_empty(data_dir: Path) -> bool:
+    """True if the data_dir is empty"""
+    return not any(data_dir.iterdir())
 
 
 def has_old_syftbox_version(data_dir: Path) -> bool:
@@ -41,7 +45,9 @@ def prompt_delete_old_data_dir(data_dir: Path) -> bool:
 def get_migration_decision(data_dir: Path):
     migrate_datasite = False
     if data_dir.exists():
-        if has_old_syftbox_version(data_dir):
+        if is_empty(data_dir):
+            migrate_datasite = False
+        elif has_old_syftbox_version(data_dir):
             # we need this extra if because we do 2 things:
             # 1. determine if we want to remove
             # 2. determine if we want to migrate
@@ -92,53 +98,12 @@ def setup_config_interactive(
         if port != conf.client_url.port:
             conf.set_port(port)
 
-    if conf.access_token is None and not skip_auth:
-        conf.access_token = authenicate_user(conf)
+    if not skip_auth:
+        conf.access_token = authenticate_user(conf)
 
     # DO NOT SAVE THE CONFIG HERE.
     # We don't know if the client will accept the config yet
     return conf
-
-
-def validate_email_token(auth_client: httpx.Client) -> str:
-    is_valid = False
-    while not is_valid:
-        email_token = Prompt.ask(
-            "[yellow]Please enter the token sent to your email. Also check your spam folder[/yellow]"
-        )
-
-        response = auth_client.post(
-            "/auth/validate_email_token",
-            headers={"Authorization": f"Bearer {email_token}"},
-        )
-
-        if response.status_code == 200:
-            is_valid = True
-            access_token = response.json()["access_token"]
-        elif response.status_code == 401:
-            rprint("[red]Invalid token, please copy the full token from your email[/red]")
-        else:
-            rprint(f"[red]An unexpected error occurred: {response.text}[/red]")
-            typer.Exit(1)
-    return access_token
-
-
-def authenicate_user(conf: SyftClientConfig) -> str:
-    auth_client = httpx.Client(base_url=str(conf.server_url))
-    response = auth_client.post(
-        "/auth/request_email_token",
-        json={"email": conf.email},
-    )
-    response.raise_for_status()
-
-    # if email_token is there, auth is disabled and we get the email_token directly
-    email_token = response.json().get("email_token", None)
-    if email_token:
-        rprint("[yellow]You are in [bold]development mode[/bold]. No email validation required.[/yellow]")
-    else:
-        access_token = validate_email_token(auth_client)
-
-    return access_token
 
 
 def prompt_data_dir(default_dir: Path = DEFAULT_DATA_DIR) -> Path:
