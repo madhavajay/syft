5754b41756fa2104e5e30dcde7cbcaca244eacce
Madhava Jay <me@madhavajay.com>
2024-11-21 09:05:38 +1000

    Temporary fix for client config serde in error report

--- syftbox/client/routers/index_router.py
+++ syftbox/client/routers/index_router.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
 from fastapi.responses import PlainTextResponse
-from syftbox.client.routers.common import APIContext
+
 from syftbox.__version__ import __version__
+from syftbox.client.routers.common import APIContext
 
 router = APIRouter()
 
